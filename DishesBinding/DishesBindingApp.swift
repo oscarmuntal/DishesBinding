1db74a3caa39627d4eae1f885d6061ded2f0ef73
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-02 19:16:58 +0200

    Initial Commit

---
+++ DishesBinding/DishesBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishesBindingApp.swift
+//  DishesBinding
+//
+//  Created by Òscar Muntal on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DishesBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
